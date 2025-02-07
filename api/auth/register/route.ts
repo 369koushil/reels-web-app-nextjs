49a1ced54942cc972224ce6a845d502bd8f1ad26
Koushil C <koushil463@gmail.com>
2025-02-08 00:00:41 +0530

    inital push

---
+++ api/auth/register/route.ts
@@ -0,0 +1,28 @@
+import { NextResponse,NextRequest } from "next/server";
+import { connectDB } from "@/libs/db";
+import User from "@/models/User";
+
+export async function POST(req:NextRequest){
+
+    try{
+        const {email,password}=await req.json()
+        if(!email||!password)return NextResponse.json({error:"email password req"},
+            {status:400}
+        )
+
+        await connectDB();
+        const existingUser=await User.findOne({email})
+        if(existingUser){
+            return NextResponse.json({
+                error:"user already registered"
+            },
+        {status:400})
+        }
+
+        const userCreated=await User.create({email,password})
+        return NextResponse.json({user:userCreated},{status:201})
+    }catch(error){
+        return NextResponse.json({error:"failed to create user"},{status:400})
+    }
+}
+
---
+++ libs/db.ts
@@ -0,0 +1,33 @@
+import mongoose, { mongo } from "mongoose";
+const MONGODB_URI=process.env.MONGODB_URI!;
+
+if(!MONGODB_URI)throw new Error("please define MONGODB_URI in .env file")
+
+let cached=global.mongoose;
+
+if(!cached){
+    cached=global.mongoose={conn:null,promise:null}
+}
+
+export async function connectDB(){
+    if(cached.conn){
+        return cached.conn
+    }
+    if(!cached.promise){
+      const opts={
+        bufferCommands:true,
+        maxPoolSize:5
+      }
+    
+    cached.promise=mongoose.connect(MONGODB_URI,opts).then(()=>{
+        return mongoose.connection
+    })
+}try{
+    cached.conn=await cached.promise;
+}catch(error){
+    cached.promise=null
+    throw error
+}
+
+return cached.conn
+}
